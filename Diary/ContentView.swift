4ce973b2ea5ef75c622ce547700edcd7bf652a60
Joe Hinkle <joehinkle11@gmail.com>
2020-06-04 15:39:28 -0500

    Initial Commit

---
+++ Diary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Diary
+//
+//  Created by Joseph Hinkle on 6/4/20.
+//  Copyright © 2020 Joseph Hinkle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
